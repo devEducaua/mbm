a7f6052a6b07a0d68e9088a45a1a5fa2d145c01e
menos <eduardo.caua@proton.me>
2026-06-24 20:16:19 -0300

    feat: move from flag mode to command

--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"os"
+	"fmt"
+	"strings"
+	"mbm/internal/commands"
+	"mbm/internal/flags"
 )
-
-type Bookmark struct {
-	Name string
-	Url string
-	Tags []string
-}
 
 func main() {
 	argv := os.Args;	
@@ -22,14 +19,70 @@ func main() {
 	}
 }
 
-func readFile(path string) (string, error) {
-	dat, err := os.ReadFile(path);
-	if err != nil {
-		if os.IsNotExist(err) {
-			return "", nil;
-		}
-		return "", fmt.Errorf("failed to read the file: %v", err);
+func parseFlags(argv []string) error {
+	if len(argv) == 0 {
+		return fmt.Errorf("no command specified");
 	}
 
-	return string(dat), nil;
+	f := make(flags.Flags);
+
+	var (
+		query string
+		file string
+		tags string
+		name string
+		verbose bool
+
+		err error
+	)
+
+	f.Submit("query", "l", &query, "make a query");
+	f.Submit("file", "f", &file, "specify file");
+	f.Submit("tags", "t", &tags, "list tags");
+	f.Submit("name", "n", &name, "specify a bookmakrs name");
+	f.Submit("verbose", "v", &verbose, "enable verbose output");
+
+	var command = argv[0];
+	switch command {
+	case "list":
+		tagsArr := strings.Split(tags, ",");
+		err = commands.ListFlag(tagsArr, verbose, query, file);
+	case "add":
+		if len(argv) < 2 {
+			return fmt.Errorf("invalid operation: `--add` needs one argument.");
+		}
+		tagsArr := strings.Split(tags, ",");
+		err = commands.AddFlag(argv[1], name, tagsArr, file);
+	case "get":
+		if len(argv) < 2 {
+			return fmt.Errorf("invalid operation: `--get` needs one argument.");
+		}
+		err = commands.GetFlag(argv[1], verbose, file);
+	case "open":
+		if len(argv) < 2 {
+			return fmt.Errorf("invalid operation: `--open` needs one argument.");
+		}
+		err = commands.OpenFlag(argv[1], file);
+	case "edit":
+		err = commands.EditFlag();
+	case "copy":
+		if len(argv) < 2 {
+			return fmt.Errorf("invalid operation: `--copy` needs one argument.");
+		}
+		err = commands.CopyFlag(argv[1], verbose, file);
+	case "import":
+		err = commands.ImportFlag(file);
+	case "help", "--help":
+		err = commands.HelpFlag();
+	}
+
+	if err != nil {
+		return err;
+	}
+
+	if err := f.Parse(argv); err != nil {
+		return err;
+	}
+	return nil;
 }
+
